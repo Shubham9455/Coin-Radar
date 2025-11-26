7949043892071ca71540e27581bcf57343356d8d
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2025-11-26 23:00:03 +0530

    compatible for sqlite

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -6,7 +6,10 @@ settings = get_settings()
 
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+'''
+check_same_thread is needed only for sqlite. It allows the database connection to be shared across multiple threads.
+'''
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
